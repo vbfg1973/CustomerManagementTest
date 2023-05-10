d39874f4895e8be2019534b01a13cd624e322eac
Chris Russell <cgrussell@gmail.com>
2023-05-10 11:39:33 +0100

    Documentation comments for Customer data model

--- src/CustomerManagement.Data/Models/Customer.cs
+++ src/CustomerManagement.Data/Models/Customer.cs
@@ -2,14 +2,44 @@
 {
     public class Customer
     {
+        /// <summary>
+        ///     Unique identifier for customer
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        ///     Customer's personal title
+        /// </summary>
         public string Title { get; set; } = null!;
+
+        /// <summary>
+        ///     Customer's first name
+        /// </summary>
         public string FirstName { get; set; } = null!;
+
+        /// <summary>
+        ///     Customer's middle names if applicable
+        /// </summary>
         public string? Middlenames { get; set; }
+
+        /// <summary>
+        ///     Customer's surname
+        /// </summary>
         public string Surname { get; set; } = null!;
 
+        /// <summary>
+        ///     Customer's addresses
+        /// </summary>
         public List<Address> Addresses { get; set; } = new();
+
+        /// <summary>
+        ///     Join tables to customer address. Default address flag is set here.
+        /// </summary>
         public List<CustomerAddress> CustomerAddresses { get; set; } = new();
+
+        /// <summary>
+        ///     Customer's contact details
+        /// </summary>
         public List<ContactDetail> ContactDetails { get; set; } = new();
     }
 }
